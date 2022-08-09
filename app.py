8bd9b2982f8ba3540bf2f652cdbf17091d355e3e
pasanjayaweera <72432510+pasanjayaweera@users.noreply.github.com>
2022-08-09 16:47:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+
+
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('nn.h5')
+  return model
+with st.spinner('Model is being loaded..'):
+  model=load_model()
+
+st.write("""
+         # Image Classification
+         """
+         )
+
+map_dict = {0: 'Class A',
+            1: 'Class B',
+            2: 'Class C',
+            3: 'Class D'}
+file = st.file_uploader("Upload the image to be classified \U0001F447", type=["jpg", "png"])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def upload_predict(upload_image, model):
+    
+        size = (64,64)    
+        image = ImageOps.fit(upload_image, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img_resize = cv2.resize(img,(64,64))
+        
+        img_reshape = img_resize[np.newaxis,...]
+    
+        prediction = model.predict(img_reshape).argmax()
+        #pred_class=decode_predictions(prediction,top=1)
+        
+        return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = upload_predict(image, model)
+    st.title("Predicted Label for the image is {}".format(map_dict [predictions]))
