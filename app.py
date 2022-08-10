d48aa78307628526e9b2937ec6c0f5bbe0ac09db
pasanjayaweera <72432510+pasanjayaweera@users.noreply.github.com>
2022-08-10 14:53:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ with st.spinner('Model is being loaded..'):
   model=load_model()
 
 st.write("""
-         # Image Classification
+         # Rust Detection
          """
          )
 
@@ -20,7 +20,8 @@ map_dict = {0: 'Class A',
             1: 'Class B',
             2: 'Class C',
             3: 'Class D'}
-file = st.file_uploader("Upload the image to be classified:", type=["jpg", "png","jpeg"])
+images = st.file_uploader("Upload the image to be classified:", type=["jpg", "png","jpeg"],
+                        accept_multiple_files= True)
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
@@ -39,12 +40,30 @@ def upload_predict(upload_image, model):
         #pred_class=decode_predictions(prediction,top=1)
         
         return prediction
-if file is None:
+if images is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, channels="RGB")
     Genrate_pred = st.button("Generate Prediction")
-    if Genrate_pred:
-        predictions = upload_predict(image, model)
-        st.title("Predicted Label for the image is {}".format(map_dict [predictions]))
+    col1,col2,col3,col4  = st.columns(4)
+    with col1:
+        st.header("Class A")
+    with col2:
+        st.header("Class B")
+    with col3:
+        st.header("Class C")
+    with col4:
+        st.header("Class D")
+    for file in images:
+        image = Image.open(file)
+        #st.image(image, channels="RGB")
+        if Genrate_pred:
+            predictions = upload_predict(image, model)
+            #st.title("Predicted Label is {}".format(map_dict [predictions]))
+            if predictions == 0:
+                col1.image(image, channels="RGB")
+            if predictions == 1:
+                col2.image(image, channels="RGB")
+            if predictions == 2:
+                col3.image(image, channels="RGB")
+            if predictions == 3:
+                col4.image(image, channels="RGB")
